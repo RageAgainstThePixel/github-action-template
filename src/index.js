bef4a7a4d24f22b6662fd74b0ffc7fb4468b6efe
Stephen Hodgson <rage.against.the.pixel@gmail.com>
2024-08-10 10:42:09 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const main = async () => {
     try {
         if (!IS_POST) {
             core.info('Hello World!');
+            core.saveState('isPost', true);
         } else {
             core.info('Hello World! (post)');
         }
